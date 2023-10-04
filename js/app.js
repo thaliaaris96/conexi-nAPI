fb2c5ee1b6db41bec49548c25d2c5d6154b59e99
Thalia Arismendi <thaliaiba96@gmail.com>
2023-10-04 13:57:36 -0300

    Actualización y corrección de css, js y hmtl

--- js/app.js
+++ js/app.js
@@ -27,18 +27,24 @@ document.addEventListener("DOMContentLoaded", function () {
                         recipes.forEach(function (recipe) {
                             const recipeName = recipe.strMeal;
                             const recipeImage = recipe.strMealThumb;
-
                             const recipeElement = document.createElement("div");
                             recipeElement.classList.add("recipe");
                             recipeElement.innerHTML = `
                                 <h3>${recipeName}</h3>
                                 <img src="${recipeImage}" alt="${recipeName}">
                             `;
+                            
+                            // Agregar evento de clic a cada elemento de receta
+                            recipeElement.addEventListener("click", function () {
+                            // Redirigir al usuario a la página de la receta completa
+                            window.location.href = `https://www.themealdb.com/meal/${recipe.idMeal}`;
+                        });
 
                             resultsContainer.appendChild(recipeElement);
                         });
                     } else {
                         resultsContainer.innerHTML = "No se encontraron recetas.";
+                        // clase "error" al elemento resultContainer
                     }
                 })
                 .catch(function (error) {
